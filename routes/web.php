7f95bd1e262bcb919812cee670332285277be45b
tareqabukwaik <103321234+tareqabukwaik@users.noreply.github.com>
2025-11-18 00:29:22 +0300

    initial push

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\PhoneNumberController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/bowling-station', [PhoneNumberController::class, 'index']);
-Route::post('/bowling-station', [PhoneNumberController::class, 'create'])
+Route::get('/', [PhoneNumberController::class, 'index']);
+Route::post('/', [PhoneNumberController::class, 'create'])
     ->name('phone.store');
